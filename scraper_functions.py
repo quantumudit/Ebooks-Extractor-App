1020cf51d774eb7dbaa567ecf916fb36f87a439c
Udit Kumar Chatterjee <quantumudit@gmail.com>
2023-10-17 03:10:45 +0530

    refactors code and add better variable names

--- scraper_functions.py
+++ scraper_functions.py
@@ -1,5 +1,5 @@
 """
-Data Scraping Helper Functions
+Data Scraping Utility Functions
 ===============================
 Author: Udit Kumar Chatterjee
 Email: quantumudit@gmail.com
@@ -8,6 +8,15 @@ Email: quantumudit@gmail.com
 This module provides a class retrieving book data from the Ebooks API.
 The functions defined is used to fetch and manipulate book data
 from the Ebooks API in a convenient way.
+
+Functions:
+----------
+1. parse_books_data(): Parse book details and return as a dictionary.
+2. get_category_subjects(): Retrieve category subjects from the Ebooks API.
+3. get_topics(): Retrieve topics for a given subject ID.
+4. total_books_present(): Retrieve the total number of books for a subject.
+5. get_books_data(): Retrieve books data for a page and subject.
+
 """
 
 
@@ -61,10 +70,9 @@ def parse_books_data(book: Dict[str, Any]) -> Dict[str, Any]:
         Dict[str, Any]: A dictionary containing the parsed details of the book.
     """
     website_url = "https://www.ebooks.com/"
-    authors = ""
     authors_data = book.get("authors")
-    if authors_data is not None:
-        authors = ", ".join([x.get("name") for x in authors_data])
+    authors = ", ".join([author.get("name")
+                        for author in authors_data]) if authors_data else ""
 
     book_url = urljoin(website_url, book.get("book_url"))
 
@@ -90,37 +98,23 @@ def get_category_subjects() -> List[Dict[str, int]]:
         Each dictionary contains the subject name as the key
         and the subject ID as the value.
     """
-    popular_cat_dict: Dict[str, int] = {}
-    fiction_cat_dict: Dict[str, int] = {}
-    non_fiction_cat_dict: Dict[str, int] = {}
+    def fetch_category_subjects(item_idx: int) -> Dict[str, int]:
+        params = [("CountryCode", "US"), ("subjectID", 184)]
+        api_base_url = "https://www.ebooks.com/api/subject/menu/"
+        user_agent = UserAgent()
+        dynamic_ua_headers = {
+            "User-Agent": user_agent.random, "accept-language": "en-US"}
+        timeout = 100
 
-    params = (
-        ("CountryCode", "US"),
-        ("subjectID", "184")
-    )
-    api_base_url = "https://www.ebooks.com/api/subject/menu/"
-    user_agent = UserAgent()
-    dynamic_ua_headers = {
-        "User-Agent": user_agent.random, "accept-language": "en-US"}
-    timeout = 100
+        response = requests.get(
+            url=api_base_url, headers=dynamic_ua_headers, params=params, timeout=timeout)
+        subject_entries = response.json(
+        )["subject_menus"][item_idx]["subject_menu_entries"]
+        return {entry["subject_name"]: entry["id"] for entry in subject_entries}
 
-    response = requests.get(
-        url=api_base_url, headers=dynamic_ua_headers, params=params, timeout=timeout
-    )
-    json_response = response.json()
-
-    popular_subjects_category = json_response["subject_menus"][1]["subject_menu_entries"]
-    fiction_category = json_response["subject_menus"][2]["subject_menu_entries"]
-    non_fiction_category = json_response["subject_menus"][3]["subject_menu_entries"]
-
-    for topic in popular_subjects_category:
-        popular_cat_dict[topic["subject_name"]] = topic["id"]
-
-    for topic in fiction_category:
-        fiction_cat_dict[topic["subject_name"]] = topic["id"]
-
-    for topic in non_fiction_category:
-        non_fiction_cat_dict[topic["subject_name"]] = topic["id"]
+    popular_cat_dict = fetch_category_subjects(item_idx=1)
+    fiction_cat_dict = fetch_category_subjects(item_idx=2)
+    non_fiction_cat_dict = fetch_category_subjects(item_idx=3)
 
     return [popular_cat_dict, fiction_cat_dict, non_fiction_cat_dict]
 
@@ -135,29 +129,21 @@ def get_topics(subject_id: int) -> Dict[str, int]:
         Dict[str, int]: A dictionary containing the topics.
         Each topic name is a key and the corresponding topic ID is the value.
     """
-    topics_dict: Dict[str, int] = {}
+    def fetch_topics(subject_id: int) -> Dict[str, int]:
+        params = [("CountryCode", "US"), ("subjectID", str(subject_id))]
+        api_base_url = "https://www.ebooks.com/api/subject/menu/"
+        user_agent = UserAgent()
+        dynamic_ua_headers = {
+            "User-Agent": user_agent.random, "accept-language": "en-US"}
+        timeout = 100
 
-    params = (
-        ("CountryCode", "US"),
-        ("subjectID", str(subject_id))
-    )
-    api_base_url = "https://www.ebooks.com/api/subject/menu/"
-    user_agent = UserAgent()
-    dynamic_ua_headers = {
-        "User-Agent": user_agent.random,
-        "accept-language": "en-US"
-    }
-    timeout = 100
-    response = requests.get(
-        url=api_base_url, headers=dynamic_ua_headers, params=params, timeout=timeout
-    )
-    subject_menus = response.json().get("subject_menus")
-    all_topics = subject_menus[0].get("subject_menu_entries")
+        response = requests.get(
+            url=api_base_url, headers=dynamic_ua_headers, params=params, timeout=timeout)
+        subject_menu = response.json().get("subject_menus")
+        return {entry["subject_name"]: entry["id"]
+                for entry in subject_menu[0]["subject_menu_entries"]}
 
-    for topic in all_topics:
-        topics_dict[topic.get("subject_name")] = topic.get("id")
-
-    return topics_dict
+    return fetch_topics(subject_id)
 
 
 def total_books_present(subject_id: int) -> int:
@@ -170,18 +156,16 @@ def total_books_present(subject_id: int) -> int:
     Returns:
         int: The total number of books present.
     """
-    params = (
+    params = [
         ("pageNumber", "1"),
         ("CountryCode", "US"),
-        ("subjectID", str(subject_id))
-    )
+        ("subjectID", str(subject_id))]
     api_base_url = "https://www.ebooks.com/api/search/subject/"
     user_agent = UserAgent()
     dynamic_ua_headers = {
-        "User-Agent": user_agent.random,
-        "accept-language": "en-US"
-    }
+        "User-Agent": user_agent.random, "accept-language": "en-US"}
     timeout = 100
+
     response = requests.get(
         url=api_base_url, headers=dynamic_ua_headers, params=params, timeout=timeout)
     total_books = response.json().get("total_results")
@@ -199,17 +183,16 @@ def get_books_data(page_num: int, subject_id: int) -> Optional[List[Dict]]:
         Optional[List[Dict]]: A list of dictionaries containing the books data.
         Returns `None` if no books data is found.
     """
-
-    params = (
+    params = [
         ("pageNumber", str(page_num)),
         ("CountryCode", "US"),
-        ("subjectID", str(subject_id))
-    )
+        ("subjectID", str(subject_id))]
     api_base_url = "https://www.ebooks.com/api/search/subject/"
     user_agent = UserAgent()
     dynamic_ua_headers = {
         "User-Agent": user_agent.random, "accept-language": "en-US"}
     timeout = 100
+
     response = requests.get(
         url=api_base_url, headers=dynamic_ua_headers, params=params, timeout=timeout)
     books_data = response.json().get("books")
